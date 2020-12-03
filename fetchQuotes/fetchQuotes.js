47c607dd4b6e470707066c6d42a8b5ebdb41ee77
davidabutler92 <davidabutler92@yahoo.com>
2020-12-03 10:14:13 -0800

    add fix fetchQuotes

--- fetchQuotes/fetchQuotes.js
+++ fetchQuotes/fetchQuotes.js
@@ -1,7 +1,7 @@
 const request = require('superagent');
 
 const fetchQuotes = async () => {
-    const { body } = await request.get('http://futuramaapi.herokuapp.com/api/quotes?')
+    const { body } = await request.get('http://futuramaapi.herokuapp.com/api/quotes/')
 
     const { character: name, quote: text, image } = body[0]
     return {
--- fetchQuotes/fetchQuotes.test.js
+++ fetchQuotes/fetchQuotes.test.js
@@ -7,7 +7,7 @@ describe('fetchQuotes', () => {
         const quote = await fetchQuotes();
         expect(quote).toEqual({
             name: 'Bender',
-            text: 'I\'m a fraud. A poor, lazy, sexy fraud.',
+            text: 'Bite my shiny metal ass.',
             image: 'https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png'
         })
     });
